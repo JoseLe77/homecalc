c497b6e22922b7f9ee447767b366e0f7629570e2
Josele <joseluisperezariza@gmail.com>
2025-02-01 11:42:30 +0100

    requerirements

--- src/homecalc.py
+++ src/homecalc.py
@@ -2,7 +2,7 @@
 # Imports
 # -------------------------------------------------------------------------
 from flask import Flask, render_template, request, redirect, url_for, session, flash, render_template_string
-import pysqlite3
+# import pysqlite3
 
 # -------------------------------------------------------------------------
 # Setup
@@ -10,11 +10,11 @@ import pysqlite3
 app = Flask(__name__)
 app.secret_key = "homecalc2025"
 
-def dbconnection():
+""" def dbconnection():
   # Connects to the specified SQLite database and returns a connection and cursor.
   connection = sqlite3.connect('../src/db/database/homecalc.db')
   cursor = connection.cursor()
-  return connection, cursor
+  return connection, cursor """
 
 """ def tmpl_show_menu():
     return render_template_string(
@@ -25,17 +25,21 @@ def dbconnection():
         '''
     ) """
 
-@app.route('/')
+""" @app.route("/")
 def home():
-    return render_template('../src/templates/base.html')
+    return render_template('../src/templates/base.html') """
 
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.5', port=5000)
+@app.route("/")
+def hello_world():
+    return render_template('/templates/app.html')
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5000)
 
 # config.py
 DEVELOPMENT = {
     'DEBUG': True,
-    'HOST': '127.0.0.5',
+    'HOST': '127.0.0.1',
     'PORT': 5000,
 }
